4a5a09ed2a652a7086e1552d643163a28a041755
Dinarhak <jjustdinar@gmail.com>
2022-12-09 01:57:26 +0300

    Вынес моковые данные о разработчиках в отдельный файл

---
+++ src/mock/sliderCards.js
@@ -0,0 +1,82 @@
+const slideCards = [
+		{
+			"full-name": "Алеша попович",
+			"photo": "./img/person-photo.png",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ligula vel leo commodo dapibus eu vel justo. Vivamus non odio mauris. Ut ornare tempus tincidunt. Duis quis nibh eget.",
+			"dev-stack": [
+				"HTML",
+				"CSS",
+				"JavaScript",
+				"React"
+			],
+			"gitHub": "",
+			"telegram": ""
+		},
+		{	
+			"full-name": "Никита Васин",
+			"photo": "./img/person-photo.png",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ligula vel leo commodo dapibus eu vel justo. Vivamus non odio mauris. Ut ornare tempus tincidunt. Duis quis nibh eget.",
+			"dev-stack": [
+				"HTML",
+				"CSS",
+				"JavaScript",
+				"React"
+			],
+			"gitHub": "",
+			"telegram": ""
+		},
+		{
+			"full-name": "Алексей Васькин",
+			"photo": "./img/person-photo.png",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ligula vel leo commodo dapibus eu vel justo. Vivamus non odio mauris. Ut ornare tempus tincidunt. Duis quis nibh eget.",
+			"dev-stack": [
+				"HTML",
+				"CSS",
+				"JavaScript",
+				"React"
+			],
+			"gitHub": "",
+			"telegram": ""
+		},
+		{
+			"full-name": "Алеша попович",
+			"photo": "./img/person-photo.png",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ligula vel leo commodo dapibus eu vel justo. Vivamus non odio mauris. Ut ornare tempus tincidunt. Duis quis nibh eget.",
+			"dev-stack": [
+				"HTML",
+				"CSS",
+				"JavaScript",
+				"React"
+			],
+			"gitHub": "",
+			"telegram": ""
+		},
+		{	
+			"full-name": "Никита Васин",
+			"photo": "./img/person-photo.png",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ligula vel leo commodo dapibus eu vel justo. Vivamus non odio mauris. Ut ornare tempus tincidunt. Duis quis nibh eget.",
+			"dev-stack": [
+				"HTML",
+				"CSS",
+				"JavaScript",
+				"React"
+			],
+			"gitHub": "",
+			"telegram": ""
+		},
+		{
+			"full-name": "Алексей Васькин",
+			"photo": "./img/person-photo.png",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ligula vel leo commodo dapibus eu vel justo. Vivamus non odio mauris. Ut ornare tempus tincidunt. Duis quis nibh eget.",
+			"dev-stack": [
+				"HTML",
+				"CSS",
+				"JavaScript",
+				"React"
+			],
+			"gitHub": "",
+			"telegram": ""
+		},
+	]
+
+export default slideCards
