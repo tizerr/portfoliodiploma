c6b46ffc156ac9a0161a9d4693f9ba9f6b4a7a64
Dinarhak <jjustdinar@gmail.com>
2022-12-09 17:50:16 +0300

    ...

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import Header from '../Header/Header.js'
+import Main from '../Main/Main.js'
+import Projects from '../Projects/Projects.js'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route index element={<Main/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+        </Routes>
+      </div>  
+    </BrowserRouter>
   );
 }
 
