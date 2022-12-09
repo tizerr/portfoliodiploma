f117babc8353cfa065cd4cb78b4e4a42d928365c
Dinarhak <jjustdinar@gmail.com>
2022-12-09 18:41:45 +0300

    Исправил опечатку

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <BrowserRouter>
-      <div className="App"}>
+      <div className="App">
         <Routes>
           <Route index element={<Main/>}/>
           <Route path="/projects" element={<Projects/>}/>
