bb21fb4c85b52d80e09fb6b47aefcefb6642c4f5
Dinarhak <jjustdinar@gmail.com>
2022-12-11 20:50:05 +0300

    Подготовил App.js для работы на <Project>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Project from '../Project/Project.jsx'
+import projects from '../../mock/projects.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <Project
+        previewPath={projects[0]['preview-path']}
+        name={projects[0]['name']}
+        developers={projects[0]['developers']}
+        kind={projects[0]['kind']}
+        description={projects[0]['description']}
+        developmentStack={projects[0]['development-stack']}
+      />
     </div>
   );
 }
