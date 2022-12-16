ba0fa77990df231a5cd88497f51779f6690f530b
Dinarhak <jjustdinar@gmail.com>
2022-12-16 21:06:14 +0300

    Прописал ссылки на соц. сети в js файле для футера

--- src/mock/footerMembers.js
+++ src/mock/footerMembers.js
@@ -4,10 +4,14 @@ export const footerMembers = [
         id: 1,
         name: 'Илья Плесский',
         tel: '+79255345543',
-        email: 'iliya@plessky.ru',
         social: {
-            git: 'https://github.com/tizerr',
-            telegram: 'https://t.me/t1z3rr'
+            git: 'tizerr',
+            telegram: 't1z3rr',
+            email: 'iliya@plessky.ru',
+        },
+        links: {
+            git: "https://github.com/tizerr",
+            telegram: "https://t.me/t1z3rr",
         }
 
     },
@@ -15,20 +19,28 @@ export const footerMembers = [
     id: 2,
     name: "Динар Фазлиев",
     tel: "+79053765012",
-    email: "jjustdinar@yandex.ru",
     social: {
+      git: "FazikkD",
+      telegram: "FazikkD",
+      email: "jjustdinar@yandex.ru",
+    },
+    links: {
       git: "https://github.com/Dinarchak",
       telegram: "https://t.me/FazikkD",
-    },
+    }
   },
   {
     id: 3,
     name: "Шахмар Абдуллаев",
     tel: "+7 965 441 26 30",
-    email: "abdull.shah@mail.ru",
     social: {
+      email: "abdull.shah@mail.ru",
       git: "abdullZiba",
-      telegram: "@abdull_shahmar",
+      telegram: "abdull_shahmar",
     },
+    links: {
+      git: "https://github.com/abdullZiba",
+      telegram: "https://t.me/abdull_shahmar",
+    }
   },
 ];
