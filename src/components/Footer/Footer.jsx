44be4aaa023d12c142af146bf3c666224595a0d8
Dinarhak <jjustdinar@gmail.com>
2022-12-18 22:05:20 +0300

    Добавил ссылки на некоторые иконки

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,7 +19,7 @@ function Footer(props) {
               <img src={telegram} alt=""/>
             </div>
           </a>
-          <a href="">
+          <a href="https://github.com/Salavat698/portfoliodiploma">
             <div className={styles.imgBlock}>
               <img src={github} alt=""/>
             </div>
@@ -29,7 +29,7 @@ function Footer(props) {
               <img src={mail} alt=""/>
             </div>
           </a>
-          <a href="">
+          <a href="https://ru.yougile.com/team/ce40cfced433/%D0%9F%D1%80%D0%B8%D0%BC%D0%B5%D1%80-%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0">
             YG
           </a>
         </div>
