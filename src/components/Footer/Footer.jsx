a80e15c29d8390d75947bd6890f1774ba668e5c1
Dinarhak <jjustdinar@gmail.com>
2022-12-18 21:47:35 +0300

    Переделал футер и перенес кнопку обратной связи

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,14 +2,45 @@ import styles from "./styles.module.css";
 import classNames from "classnames";
 import { footerMembers } from "../../mock/footerMembers";
 import { FooterItem } from "../FooterItem/FooterItem";
+import { FeedbackModul } from "../FeedbackModul/FeedbackModul";
+import telegram from '../../img/telegram-icon.svg'
+import github from '../../img/github-icon.svg'
+import mail from '../../img/mail_icon.svg'
 
 function Footer(props) {
   console.log(footerMembers);
   return (
     <footer className={classNames(styles.footer)}>
-      {footerMembers.map((person, key) => (
+      <div>
+        <p>Арбузики</p>
+        <div className={styles.links}>
+          <a href="">
+            <div className={styles.imgBlock}>
+              <img src={telegram} alt=""/>
+            </div>
+          </a>
+          <a href="">
+            <div className={styles.imgBlock}>
+              <img src={github} alt=""/>
+            </div>
+          </a>
+          <a href="">
+            <div className={styles.imgBlock}>
+              <img src={mail} alt=""/>
+            </div>
+          </a>
+          <a href="">
+            YG
+          </a>
+        </div>
+      </div>
+      <div className={styles.feedback}>
+        <button className={styles.egg}>Посхалка</button>
+        <FeedbackModul/>
+      </div>
+            {/*{footerMembers.map((person, key) => (
         <FooterItem key={key} info={person} />
-      ))}
+      ))}*/}
     </footer>
   );
 }
