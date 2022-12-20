87651186f01e3c507e7951fb8d8de72e618ff105
Dinarhak <jjustdinar@gmail.com>
2022-12-20 19:30:34 +0300

    Указал цвет для спинера в темной и светлой темах

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Main from "../Main/Main.js";
 import Projects from "../Projects/Projects.jsx";
 import Footer from "../Footer/Footer.jsx";
-
+import { Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeWrapper } from "../ThemeWrapper/ThemeWrapper";
 
