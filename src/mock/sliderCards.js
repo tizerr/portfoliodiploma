961a339886a5a2b6f8e9efbfd1aa0dd2e513c6a5
ilya159368 <72142468+ilya159368@users.noreply.github.com>
2022-12-22 17:45:46 +0300

    person photo

--- src/mock/sliderCards.js
+++ src/mock/sliderCards.js
@@ -2,7 +2,7 @@ const slideCardsRu = [
 
   {
     "full-name": "Фазлиев Динар",
-    "photo": "./img/projects/person-photo.png",
+    "photo": "./img/members/person-photo.png",
     "description":"Я - тимлид команды. Web-разработка - одно из моих хобби, которое сильно преобразилось после Интенсива. Кроме этого меня интересует аналитика данных,но для нее нужно чуть-чуть поумнеть:)",
     "gitHub": "https://github.com/Dinarchak",
     "dev-stack": ["HTML", "JavaScript", "CSS", "SCSS", "React", "BS4"],
@@ -10,7 +10,7 @@ const slideCardsRu = [
   },
   {
     "full-name": "Шахмар Абдуллаев",
-    "photo": "./img/projects/shah-person-photo.png",
+    "photo": "./img/members/shah-person-photo.png",
     "description":"Меня зовут Шахмар! Я ученик It-класса. В настоящее время популярность It-сферы развивается с огромной скоростью. И я стараюсь не отставать!",
     "dev-stack": ["HTML", "CSS", "JavaScript", "React"],
     "gitHub": "https://github.com/abdullZiba",
@@ -18,10 +18,7 @@ const slideCardsRu = [
   },
   {
     "full-name": "Илья Плесский",
-    "photo": "./img/projects/person-photo.png",
-
-    "description":"Меня зовут Илья! Я ученик It-класса. В настоящее время популярность It-сферы развивается с огромной скоростью. И я стараюсь не отставать!",
-
+    "photo": "./img/members/ilya.png",
     "description": "Программирование - мое хобби. Я пробую разные IT направления, чтобы найти ту область, с которой я свяжу свою жизнь",
     "dev-stack": ["HTML", "CSS", "JavaScript", "React"],
     "gitHub": "https://github.com/tizerr",
@@ -33,7 +30,7 @@ const slideCardsEn = [
 
   {
     "full-name": "Fazliev Dinar",
-    "photo": "./img/projects/person-photo.png",
+    "photo": "./img/members/person-photo.png",
     "description":"I am the team's team leader. Web-development is one of my hobbies, which has changed a lot after the Intensive. Apart from that I am interested in data analytics, but for that I need to get a little smarter:)",
     "gitHub": "https://github.com/Dinarchak",
     "dev-stack": ["HTML", "JavaScript", "CSS", "SCSS", "React", "BS4"],
@@ -41,7 +38,7 @@ const slideCardsEn = [
   },
   {
     "full-name": "Shahmar Abdullayev",
-    "photo": "./img/projects/shah-person-photo.png",
+    "photo": "./img/members/shah-person-photo.png",
     "description":"My name is Shakhmar! I am a student of the It-sphere. Nowadays, the popularity of the It-sphere is developing at a tremendous rate. And I'm trying to keep up!",
     "dev-stack": ["HTML", "CSS", "JavaScript", "React"],
     "gitHub": "https://github.com/abdullZiba",
@@ -49,7 +46,7 @@ const slideCardsEn = [
   },
   {
     "full-name": "Plessky Ilya",
-    "photo": "./img/projects/person-photo.png",
+    "photo": "./img/members/ilya.png",
     "description": "Programming is my hobby. I'm trying out different IT fields to find the one I want to do with my life.",
 
     "dev-stack": ["HTML", "CSS", "JavaScript", "React"],
